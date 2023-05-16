154402e6cc9e4df733dfb607f9ba61964cdb449e
ntsambani-fms <ntsambani@fmsystems.com>
2023-05-16 19:22:23 +0200

    display generes

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,20 @@
 
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
   const GenreList = () => {
     const {data} = useGenres();
   
     return (
-        <ul>
-            {data.map(data => <li key={data.id}>{data.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
+                <HStack>
+                    <Image boxSize='32px'  borderRadius={8} src={getCroppedImageUrl(genre.image_background)}></Image>
+                    <Text fontSize='lg'>{genre.name}</Text>
+                </HStack>
+            </ListItem>)}
+        </List>
     )
     }
   export default GenreList;
