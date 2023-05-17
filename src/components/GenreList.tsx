51a143ccfae5760dc686a59cc2f952667858f655
ntsambani-fms <ntsambani@fmsystems.com>
2023-05-17 18:07:10 +0200

    shift genres with the app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,9 +11,6 @@ interface Props{
   const GenreList = ({selectedGenre, onSelectGenre}: Props) => {
     const {data, isLoading, error} = useGenres();
 
-    if(error) return null;
-    if(isLoading) return <Spinner />
-  
     return (
         <><Heading fontSize='2xl' marginBottom={3}>Genres</Heading><List>
             {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
